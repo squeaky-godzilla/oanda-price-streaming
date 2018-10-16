64121a0583d78e169b97ceb2cfe4e6f5da02ef5b
Vitek <Vitek@rig.home>
2018-10-16 23:26:15 +0100

    dataframe load wip

---
+++ stream-price-df.py
@@ -0,0 +1,90 @@
+import argparse
+import json
+import pandas as pd
+from oandapyV20 import API
+from oandapyV20.exceptions import V20Error, StreamTerminated
+from oandapyV20.endpoints.pricing import PricingStream
+# from exampleauth import exampleAuth
+from requests.exceptions import ConnectionError
+
+'''
+time_point format:
+
+{
+  "type": "PRICE",
+  "time": "2018-10-16T22:22:35.397047287Z",
+  "bids": [
+    {
+      "price": "1.15732",
+      "liquidity": 10000000
+    }
+  ],
+  "asks": [
+    {
+      "price": "1.15751",
+      "liquidity": 10000000
+    }
+  ],
+  "closeoutBid": "1.15717",
+  "closeoutAsk": "1.15766",
+  "status": "tradeable",
+  "tradeable": true,
+  "instrument": "EUR_USD"
+}
+'''
+
+# create the top-level parser
+parser = argparse.ArgumentParser(prog='steaming_prices')
+parser.add_argument('--timeout', default=0, type=float,
+                    help='timeout in secs., default no timeout')
+parser.add_argument('--count', default=0, type=int,
+                    help='# of records to receive, default = unlimited.')
+parser.add_argument('--instruments', type=str, nargs='?',
+action='append', help='instruments')
+
+args = parser.parse_args()
+
+max_records = args.count
+
+request_params = {}
+
+if args.timeout:
+    request_params = {"timeout": args.timeout}
+
+with open('token') as token_file:
+    auth_token_data = json.load(token_file)
+
+
+api = API(access_token=auth_token_data['oanda-api-token'],
+          environment="practice",
+          request_params=request_params)
+
+r = PricingStream(accountID=auth_token_data['oanda-account-id'], params={"instruments": ",".join(args.instruments)})
+
+price_df = pd.DataFrame(columns=[])
+
+n = 0
+while True:
+    try:
+        for time_point in api.request(r):
+            print(time_point)
+            n += 1
+            if max_records and n >= max_records:
+                r.terminate("maxrecs received: {}".format(max_records))
+
+    except V20Error as e:
+        # catch API related errors that may occur
+        with open("LOG", "a") as LOG:
+            print("V20Error: {}\n".format(e))
+        break
+    except ConnectionError as e:
+        with open("LOG", "a") as LOG:
+            print("Error: {}\n".format(e))
+    except StreamTerminated as e:
+        with open("LOG", "a") as LOG:
+            print("Stopping: {}\n".format(e))
+        break
+    except Exception as e:
+        with open("LOG", "a") as LOG:
+            print("??? : {}\n".format(e))
+        break
